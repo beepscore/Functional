8623059681878a5a1ecdf3e2c5fb121db1243abf
beepscore <support@beepscore.com>
2016-04-02 10:31:18 -0700

    Add functional style filter(isEven)

--- Functional.playground/Contents.swift
+++ Functional.playground/Contents.swift
@@ -11,3 +11,13 @@ for i in 1...10 {
     }
 }
 print(evens)
+
+// functional style
+func isEven(number: Int) -> Bool {
+   return number % 2 == 0
+}
+
+// filter argument is boolean function isEven
+// tutorial says "functions are just named closures"
+evens = Array(1...10).filter(isEven)
+print(evens)
